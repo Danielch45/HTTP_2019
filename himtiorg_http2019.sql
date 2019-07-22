b88fd7c9c7876b0a3535db6fbcd8e15a2614fea8
roche15 <34719831+roche15@users.noreply.github.com>
2019-07-22 13:02:29 +0700

    Database, Project File, Cara Pemakaian
    Project File, Frontend yang dipakai masih raw, jadi untuk backend sudah bisa dikerjakan sekarang

---
+++ himtiorg_http2019.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 22, 2019 at 12:57 PM
+-- Server version: 10.2.25-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `himtiorg_http2019`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registerant`
+--
+
+CREATE TABLE `registerant` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nim` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `line` varchar(255) NOT NULL,
+  `jurusan` varchar(255) NOT NULL,
+  `batch` varchar(255) NOT NULL,
+  `region` varchar(255) NOT NULL,
+  `transport` varchar(255) NOT NULL,
+  `vege` varchar(255) NOT NULL,
+  `baju` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `registerant`
+--
+ALTER TABLE `registerant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `registerant`
+--
+ALTER TABLE `registerant`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
